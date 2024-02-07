207c1e55fe37e1b0243bad69fa69c167d7abd607
gabriel <gabriel.aguiar@smarthive.com.br>
2024-02-07 02:31:25 -0300

    configurações iniciais

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.tsx";
+import { GlobalStyle } from "./styles/global.ts";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
+  </React.StrictMode>
+);
